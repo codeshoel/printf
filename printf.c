e5aa9195631e7a3faf3a40c097504f46fd0fc76c
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-10-16 01:01:47 +0000

    _printf

--- printf.c
+++ printf.c
@@ -1,19 +1,59 @@
 #include main.h
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
 /***
-* Description: function to produce output according to a format
+* Description: _printf - function to produce output according to a format
+* @format: String with format specifier
 * Returns: the number of characters printed;
 */
 
 
 int _printf(const char *format, ...)
 {
-	char ptr = &format;
+	if (format != NULL)
+	{
+		int count = 0, i;
+		int (*m)(va_list);
+		va_list args;
 
-	printf("%c", ptr);
-	printf("%s", ptr);
+		va_start(args, format);
+		i = 0;
 
-	return (0);
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+		while (format != NuLL && format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _printchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					m = get_func(format[i + 1]);
+					if (m)
+						count += m(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i+1]);
+					i += 2;
+				}
+			}
+			else
+			{
+				count += _putchar(format[i]);
+				i++;
+			}
+		}
+		va_end(args);
+		return (count);
+
+	}
+
+	return (-1);
 
 }
 
