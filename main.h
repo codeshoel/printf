c2a79a9620e8cec62eacf82601cbd8af3f11fa68
Gabriel-J-softmaktech <Gsoftmaktech@gmail.com>
2022-10-16 01:11:02 +0000

    _printf

--- main.h
+++ main.h
@@ -1,5 +1,16 @@
 #ifdef AL_X_INCLUDED
 #define AL_X_INCLUDED
+#define our_printf
+#ifndef our_printf
+#include <stdio.h>
+#include <stdarg.h>
+
+
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
 
 int _printf(const char *format, ...);
 
